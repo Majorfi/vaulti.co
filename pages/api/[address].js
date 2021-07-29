04134fb67fb8c3e039d2e7a06d7f29cfca472902
Thomas Bouder <tbouder@protonmail.com>
2021-07-29 20:16:28 +0200

    ✨ Adding multi-vault fetch

--- pages/api/[address].js
+++ pages/api/[address].js
@@ -1,6 +1,6 @@
 import	axios			from	'axios';
 
-export const	performGet = (url) => {
+const	performGet = (url) => {
 	return (
 		axios.get(url)
 			.then(function (response) {
@@ -13,28 +13,67 @@ export const	performGet = (url) => {
 	);
 };
 
-const	mapping = {};
-export default async function handler(req, res) {
-	const	{address, revalidate} = req.query;
-	const	now = new Date().getTime();
+async function asyncForEach(array, callback) {
+	for (let index = 0; index < array.length; index++) {
+		await callback(array[index], index, array);
+	}
+}
 
-	if (mapping[address] !== undefined) {
-		const	lastAccess = mapping[address]._VAULTI_LAST_ACCESS_;
-		if (((now - lastAccess) > 10 * 60 * 1000) || revalidate === 'true') {
-			const	vaults = await performGet('https://api.yearn.tools/vaults/all');
-			const	vault = vaults.find(v => v.address.toLowerCase() === address.toLowerCase());
-			mapping[address] = vault;
-			mapping[address]._VAULTI_LAST_ACCESS_ = now;
-			res.status(200).json(vault)
-		} else {
-			mapping[address]._VAULTI_LAST_ACCESS_ = now;
-			res.status(200).json(mapping[address]);
-		}
-	} else {
+const	mapping = {};
+
+async function fetchAndGetAddress(address) {
+	const	now = new Date().getTime();
+	const	vaults = await performGet('https://api.yearn.tools/vaults/all');
+	const	vault = vaults.find(v => address === v.address.toLowerCase());
+	if (!vault) {
+		return ({code: 404, json: {}});
+	}
+	mapping[address] = vault;
+	mapping[address]._VAULTI_LAST_ACCESS_ = now;
+	return ({code: 200, json: mapping[address]});
+}
+async function checkAndGetAddress(address, revalidate) {
+	const	now = new Date().getTime();
+	const	lastAccess = mapping[address]._VAULTI_LAST_ACCESS_;
+	if (((now - lastAccess) > 10 * 60 * 1000) || revalidate === 'true') {
 		const	vaults = await performGet('https://api.yearn.tools/vaults/all');
-		const	vault = vaults.find(v => v.address.toLowerCase() === address.toLowerCase());
+		const	vault = vaults.find(v => address === v.address.toLowerCase());
+		if (!vault) {
+			return ({code: 404, json: {}});
+		}
 		mapping[address] = vault;
 		mapping[address]._VAULTI_LAST_ACCESS_ = now;
-		res.status(200).json(vault)
+		return ({code: 200, json: vault});
 	}
+	mapping[address]._VAULTI_LAST_ACCESS_ = now;
+	return ({code: 200, json: mapping[address]});
+}
+async function getAddress(address, revalidate) {
+	if (mapping[address] !== undefined) {
+		return await checkAndGetAddress(address, revalidate);
+	} else {
+		return await fetchAndGetAddress(address);
+	}
+}
+
+export default async function handler(req, res) {
+	let		{address, revalidate} = req.query;
+	const	addresses = address.toLowerCase().split(',')
+	address = address.toLowerCase();
+
+	if (addresses.length === 0) {
+		return res.status(404).json({});
+	}
+	if (addresses.length === 1) {
+		const	{code, json} = await getAddress(address, revalidate)
+		return	res.status(code).json(json);
+	}
+	//so if addresses > 1
+	const	responseBuilder = {};
+	await asyncForEach(addresses, (async (addr) => {
+		const	{json} = await getAddress(addr, revalidate)
+		responseBuilder[addr] = json;
+	}));
+	return	res.status(200).json(responseBuilder);
+	
 }
